aa885a817926d726226df6ba4ed18536838330b9
klew3509 <33301853+klew3509@users.noreply.github.com>
2017-11-02 08:48:57 -0400

    updates change design

--- Design 2.py
+++ Design 2.py
@@ -1,30 +1,14 @@
 import turtle
 from MyShape import *
 turtle.colormode(255)
-turtle.bgcolor("black")
+turtle.bgcolor("red")
 turtle.tracer()
 
 bob=turtle.Turtle()
 bob.speed(0)
 
-for times in range(606):
-    bob.color(255, 69, 0)
-    bob.width(6)
-    bob.circle(300000)
-    bob.right(25)
-
-for times in range(35):
-    bob.width(5)
-    bob.color(255, times + 10, 0)
-    bob.circle(150)
-    bob.right(55)
-    bob.circle(55555555)
-    bob.left(25)
-
-bob.color(128 - times, 255 - times, 0)
+background(bob, 50)
 move(bob, 0, 0)
-square(bob, 20)
-
-
-
+bob.width(20)
+dome(bob, 100, 25)
 
